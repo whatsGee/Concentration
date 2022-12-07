9550335cfb49d8dff7c60de6d216de6cc3b95dd9
whatsGee <gadejesus09@gmail.com>
2022-12-06 17:57:04 -0800

    Initial commit

---
+++ app/src/main/java/com/example/concentration/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.concentration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView title = findViewById(R.id.titleName);
+        title.setText("Concentration");
+    }
+}
